5441dbb9a2074d02053e59f29c5b4b9b12a07d77
prosper <mprosp3r@gmail.com>
2023-06-07 01:29:30 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(0, 100):
     if i < 10 or i == 99:
         print( 0 if i < 10 else 99, end="" if i < 10 else "\n")
     if i != 99:
-        print(f"{i}, ", end="")
+        print("{:d}, ".format(i), end="")
