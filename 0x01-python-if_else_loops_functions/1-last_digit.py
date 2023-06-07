adf76cea06d09fbf4f06f4f955187759b1faa511
prosper <mprosp3r@gmail.com>
2023-06-06 17:47:01 -0700

    1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print(f"Last digit of {number} ", end="")
+if number < 0:
+    number = -number
+    number = number % 10
+    number = -number
+else:
+    number = number % 10
+print(f"is {number} ", end="")
+if number > 5:
+    print("and is great than 5")
+elif number == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
