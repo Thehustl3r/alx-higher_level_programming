ebfb11371934d97796c263aaf3d4f5b1846fbaba
prosper <mprosp3r@gmail.com>
2023-06-07 13:47:44 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,11 @@
 def uppercase(str):
     newstring = []
     for i in range(len(str)):
-        test = str[i]
+        test = ord(str[i])
         for j in range(97, 123):
             if test == j:
                 test = test - 32
+                print("{:c}".format(test), end ="")
                 break
         newstring.append(chr(test))
-    print("{}".format(newstring))
+    print("\n")
