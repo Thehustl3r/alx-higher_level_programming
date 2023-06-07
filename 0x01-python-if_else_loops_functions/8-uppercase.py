1eabc868d4d0364d8ed5e4a475edb511c7a3a6c0
prosper <mprosp3r@gmail.com>
2023-06-07 03:19:11 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        test = str[i]
+        for j in range(97, 123):
+            if test == j:
+                test = test - 32
+                break
+        str[i] = chr(test)
