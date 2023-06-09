b69050ea77ba0f0abf8813dd9464e907c0638319
prosper <mprosp3r@gmail.com>
2023-06-08 17:36:20 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 if __name__ == "__main__":
     ac = len(argv) - 1
-    if ac < 4:
+    if ac < 3:
         print("{} <a> <operator> <b>".format(argv[0]))
         exit(1)
     else:
