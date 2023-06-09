afd3bbcd6288302ee16103f6b24ac03243ea3841
prosper <mprosp3r@gmail.com>
2023-06-08 17:58:08 -0700

    103-fast_alphabet.py

---
+++ 0x02-python-import_modules/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    print("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    n = len(str) - 1
+    for i in range(len(str)):
+        test = ord(str[i])
+        if test > 96 and test < 123:
+            for j in range(97, 123):
+                if test == j:
+                    test = test - 32
+                    print("{:c}".format(test), end="" if i != n else "\n")
+                    break
+        else:
+            print("{:c}".format(test), end="" if i != n else "\n")
