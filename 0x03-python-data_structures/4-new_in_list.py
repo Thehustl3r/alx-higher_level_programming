e9d50c0f37a2ea62236248777e4ef0014d9e558f
prosper <mprosp3r@gmail.com>
2023-06-11 23:50:00 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx > len(my_list) - 1 or idx < 0:
-        return (my_list);
-    new_list = [];
+        return (my_list)
+    new_list = []
     for i in my_list:
         new_list.append(i)
     new_list[idx] = element
