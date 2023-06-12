ad885e29688b2805344f95e9e065658e58f20a0d
prosper <mprosp3r@gmail.com>
2023-06-12 00:30:50 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print("{:d} ".format(j), end="")
-        print()
-
+        for a, j in enumerate(i):
+            if a != len(i) - 1:
+                print("{:d} ".format(j), end="")
+            else:
+                print("{:d}".format(j))
