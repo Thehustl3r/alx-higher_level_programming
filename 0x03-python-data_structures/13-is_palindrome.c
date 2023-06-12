d72bb53f06f70be1c2a39b51bdf17eb02bbdfa8a
prosper <mprosp3r@gmail.com>
2023-06-12 16:59:55 -0700

    m

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * is_palindrome -  the functio that test if a list is palindrome
+ * @head: pointer to the list
+ * Return: 1 is palindrome elese 0
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current;
+
+	current = *head;
+	if (!current)
+		return (0);
+	return (1);
+}
