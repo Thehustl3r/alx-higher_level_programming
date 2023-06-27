9485fb8190a98a3ad7686e709712b82f7a2f3081
prosper <mprosp3r@gmail.com>
2023-06-27 11:04:46 -0700

    addition requirements

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,14 @@
 #!/bin/usr/python3
 # -*- coding: utf-8 -*-
+
+"""
+Created on Tue June 27 18:54:23 2023
+@author: Prosper Mugisha
+"""
 class Square:
     """Empty class Sqare that defines a square
 
     Attributes:
         empty
     """
-
-    __dict__ = {}
+    pass
