bb07a00ff9a303f776e68d6b32ac47fb5e2d3f32
prosper <mprosp3r@gmail.com>
2023-07-12 15:16:19 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Created on """
+
+
+class MyList(list):
+        """class MyLi that inherits from list"""
+         def print_sorted(self):
+                """Public instance method that prints sorted list"""
+    list_copy = self[:]
+    list_copy.sort()
+    print(list_copy)
