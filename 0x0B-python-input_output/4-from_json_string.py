c2d6e913e2b7e0c102b44a14da4dc0687a4fc35c
prosper <mprosp3r@gmail.com>
2023-07-13 06:40:35 -0700

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,9 +5,4 @@ import json
 
 def from_json_string(my_str):
     """ function that converts json to string"""
-    parsed_obj = json.loads(my_str)
-    if isinstance(parsed_obj, dict):
-        sorted_obj = dict(sorted(parsed_obj.items()))
-    if isinstance(parsed_obj, list):
-        sorted_obj = sorted(parsed_obj)
-    return sorted_obj
+    return json.loads(my_str)
