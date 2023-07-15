7c671c18633f1399222f2cf0b420f893506ed462
prosper <mprosp3r@gmail.com>
2023-07-15 05:27:39 -0700

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Created by mugisha prosper"""
+
+
+def is_same_class(obj, a_class):
+    """ The function the check wheathe instance in certain class """
+    if (isinstance(obj, a_class)):
+        return True
+    else:
+        return False
