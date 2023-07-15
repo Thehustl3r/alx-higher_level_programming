e19bbd04e4b74c878849c327b634510f33683ec5
prosper <mprosp3r@gmail.com>
2023-07-15 03:42:02 -0700

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,9 +3,9 @@
 
 
 class MyList(list):
-        """class MyLi that inherits from list"""
-         def print_sorted(self):
-                """Public instance method that prints sorted list"""
-    list_copy = self[:]
-    list_copy.sort()
-    print(list_copy)
+    """class MyLi that inherits from list"""
+    def print_sorted(self):
+        """Public instance method that prints sorted list"""
+        list_copy = self[:]
+        list_copy.sort()
+        print(list_copy)
