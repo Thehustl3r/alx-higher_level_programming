a62159267af5c3695cfa66a6b8cd601785169eb8
Mugisha Prosper <mprosp3r@gmail.com>
2023-07-21 09:33:21 -0700

    community

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Create by Mugisha Prosper"""
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -62,3 +64,71 @@ class Base:
         list_dicts = json.loads(json_obj)
         instance = [cls.create(**dict_obj) for dict_obj in list_dicts]
         return instance
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ save to filr csv function"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            if list_objs is not None:
+                for obj in list_objs:
+                    if cls.__name__ == "Rectangle":
+                        writer.writerow([obj.id,
+                                         obj.width,
+                                         obj.height,
+                                         obj.x, obj.y])
+                    elif cls.__name__ == "Square":
+                        writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ load from file from csv"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, newline='') as file:
+                reader = csv.reader(file)
+                data = [list(map(int, row)) for row in reader]
+                if cls.__name__ == "Rectangle":
+                    fields = ["id", "width", "height", "x", "y"]
+                elif cls.__name__ == "Square":
+                    fields = ["id", "size", "x", "y"]
+                return [cls(**dict(zip(fields, row))) for row in data]
+        except FileNotFoundError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """draw function"""
+        screen = turtle.Screen()
+        screen.setup(800, 600)
+        t = turtle.Turtle()
+        t.speed(1)
+
+        for rect in list_rectangles:
+            t.penup()
+            t.goto(rect.x, rect.y)
+            t.pendown()
+            t.forward(rect.width)
+            t.left(90)
+            t.forward(rect.height)
+            t.left(90)
+            t.forward(rect.width)
+            t.left(90)
+            t.forward(rect.height)
+            t.left(90)
+
+        for square in list_squares:
+            t.penup()
+            t.goto(square.x, square.y)
+            t.pendown()
+            t.forward(square.size)
+            t.left(90)
+            t.forward(square.size)
+            t.left(90)
+            t.forward(square.size)
+            t.left(90)
+            t.forward(square.size)
+            t.left(90)
+
+        turtle.done()
