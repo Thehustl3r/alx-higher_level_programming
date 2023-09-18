2f2f7acde0d9b2e1906350b15c95c0e7b56617c8
prosper <mprosp3r@gmail.com>
2023-09-18 08:03:39 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+"""
+created by Mugisha Prosper
+at Fri, 15,09,23
+"""
 import MySQLdb
 import sys
-"""
-    created by Mugisha Prosper
-    at Fri, 15,09,23
-"""
 
 
 if __name__ == "__main__":
