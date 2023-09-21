0760b47f244720c5cf089c6767febbeb8c3e66bd
prosper <mprosp3r@gmail.com>
2023-09-21 03:48:00 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
     orignal_string = sys.argv[4]
     pattern = r"[;]"
     query = re.sub(pattern, "", orignal_string)
-    print(query)
     db = MySQLdb.connect(host="localhost", port=3306, user=my_user,
                          passwd=password, db=db_name, charset="utf8")
     cur = db.cursor()
