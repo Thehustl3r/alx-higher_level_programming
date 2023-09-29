9069d5172d359ba98720a9901bdbdb2c5616f702
prosper <mprosp3r@gmail.com>
2023-09-29 11:59:25 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,5 +4,8 @@ import urllib.request
 
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
-    body = response.read()
-    print(body)
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: ", html.decode('utf-8'))
