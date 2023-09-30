b4f65abca28b8a5f330b0b40b03821175d17e990
prosper <mprosp3r@gmail.com>
2023-09-29 18:28:12 -0700

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module tends to fetch data from database """
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = {'email': argv[2]}
+    response = post(url, data=email)
+    print(response.text)
