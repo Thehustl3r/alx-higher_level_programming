7beac7a218ed547556b4a595de765488dc1e383c
prosper <mprosp3r@gmail.com>
2023-09-29 19:56:20 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,13 +6,10 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    if (len(argv) != 2):
-        exit(1)
-
     url = argv[1]
-    with urlopen(url) as response:
-        try:
+    try:
+        with urlopen(url) as response:
             the_page = response.read().decode('utf-8')
             print(the_page)
-        except HTTPError as e:
-            print('Error code: {}'.format(e.code))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
