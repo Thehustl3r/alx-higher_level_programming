0e2833cdc47d94e2db562c5b08b40e668be2cfb3
prosper <mprosp3r@gmail.com>
2023-09-29 18:09:43 -0700

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" fetch code with errors"""
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    if (len(argv) != 2):
+        exit(1)
+
+    url = argv[1]
+    with urlopen(url) as response:
+        try:
+            the_page = response.read().decode('utf-8')
+            print(the_page)
+        except HTTPError as e:
+            print("Error code:", e.code)
