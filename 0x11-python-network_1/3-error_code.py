aa0f2954d24b0897705cb97aa84ffbeed2c94ec3
prosper <mprosp3r@gmail.com>
2023-09-29 18:13:02 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
             the_page = response.read().decode('utf-8')
             print(the_page)
         except HTTPError as e:
-            print("Error code:", e.code)
+            print('Error code: {}'.format(e.code))
