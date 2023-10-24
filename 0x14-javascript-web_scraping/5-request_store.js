b5b50cf1efe469088e280ba8aba7a3a0f2a038ed
prosper <mprosp3r@gmail.com>
2023-10-24 15:58:13 -0700

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) { process.exit(1); } else if (response.statusCode !== 200) { process.exit(1); } else {
+    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {});
+  }
+}
+);
